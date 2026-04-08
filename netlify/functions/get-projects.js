8d0c887cd0cde425b8d9979d35867ffcc217cc72
billhickner <bhickner@gmail.com>
2026-04-08 18:07:08 -0400

    Update get-projects.js

--- netlify/functions/get-projects.js
+++ netlify/functions/get-projects.js
@@ -30,7 +30,6 @@ exports.handler = async (event) => {
       var includeAll = event.queryStringParameters && event.queryStringParameters.all === 'true';
       if (!includeAll) {
         data = (Array.isArray(data) ? data : []).filter(function(p) { return !String(p.id).startsWith('_'); });
-      });
       }
       return { statusCode: 200, headers: CORS, body: JSON.stringify({ projects: data }) };
     } catch (e) {
